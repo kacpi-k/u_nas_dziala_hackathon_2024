d81d88a31387e9ebd56eca72bfe90f2149c1b659
Rafikssss <bartosiewiczrafal5@gmail.com>
2024-10-12 19:49:14 +0200

    Register Page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:u_nas_dziala_hackathon_2024/core/configs/theme/app_theme.dart';
 import 'package:u_nas_dziala_hackathon_2024/firebase_options.dart';
+import 'package:u_nas_dziala_hackathon_2024/presentation/auth/pages/register.dart';
 import 'package:u_nas_dziala_hackathon_2024/presentation/home/pages/home.dart';
 import 'package:u_nas_dziala_hackathon_2024/service_locator.dart';
 import 'package:u_nas_dziala_hackathon_2024/presentation/auth/pages/login.dart';
@@ -23,7 +24,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.appTheme,
-      home: const HomePage(),
+      home: const RegisterPage(),
     );
   }
 }
