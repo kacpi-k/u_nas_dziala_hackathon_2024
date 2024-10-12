cf0765e7fef807b3f178d8b722fc1a461892853b
Mariusz Sikora <maniek@MacBook-Pro-Mariusz.local>
2024-10-12 12:23:36 +0200

    Firebase config and project structure done

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // TODO KOLORKI
+  static const primary = Color(0xff8E6CEF);
+  static const background = Color(0xff1D182A);
+  static const secondBackground = Color(0xff342F3F);
+}
