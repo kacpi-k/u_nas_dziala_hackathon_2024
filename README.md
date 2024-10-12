5a08ebef34e2872804ec4c0c145ec914c2284b46
Mariusz Sikora <maniek@MacBook-Pro-Mariusz.local>
2024-10-12 10:59:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# u_nas_dziala_hackathon_2024
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/u_nas_dziala_hackathon_2024/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_nas_dziala_hackathon_2024
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
