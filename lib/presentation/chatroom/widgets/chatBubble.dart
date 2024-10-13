3cb39aecc9fac5c9930fe64b8a799b19085ff322
Rafikssss <bartosiewiczrafal5@gmail.com>
2024-10-13 08:28:59 +0200

    Messenger fixed

---
+++ lib/presentation/chatroom/widgets/chatBubble.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:u_nas_dziala_hackathon_2024/core/configs/theme/app_colors.dart';
+
+class Chatbubble extends StatelessWidget {
+  final String message;
+  const Chatbubble({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: AppColors.primary
+        ),
+        child: Text(message, style: TextStyle(color: Colors.black54)),
+    );
+  }
+}
